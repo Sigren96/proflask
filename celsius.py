bed2db6afc9ba40fa09689c2c9daece925104402
Juan David <juandavidenriquez602@gmail.com>
2026-05-01 21:39:05 -0500

    se agrego import os

--- celsius.py
+++ celsius.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template
 from flask_cors import CORS 
 import numpy as np
 import tensorflow as tf
+import os
 # ========================= 
 # # CONFIG
 # =========================
@@ -35,6 +36,7 @@ def predict_get(celsius):
 # ========================= # MAIN
 # =========================
 
-app.run(debug=True)
-
-
+if __name__ == "__main__": 
+    # Esto obliga a Flask a usar la puerta pública (0.0.0.0) y el puerto de Render
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
